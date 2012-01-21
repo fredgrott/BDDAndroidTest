2c350eb8e7aeb89a5fd287a4f94bd7ed60cbf113
Fred Grott <fred.grott@gmail.com>
2012-01-21 17:24:18 -0600

    some changes to first test and removed guice-no-aop, javax-inject, and roboguice2b3 from libs until further along

--- src/com/github/shareme/bddandroid/test/BDDAndroidActivityTest.java
+++ src/com/github/shareme/bddandroid/test/BDDAndroidActivityTest.java
@@ -1,40 +1,57 @@
 package com.github.shareme.bddandroid.test;
 
-import com.github.shareme.bddandroid.BDDAndroidActivity;
+import com.github.shareme.bddandroid.*;
+import com.github.shareme.bddandroid.R;
 import com.jayway.android.robotium.solo.*;
+
+import android.content.Context;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
+import android.util.AttributeSet;
 
 // TODO: Auto-generated Javadoc
 /**
  * The Class BDDAndroidActivityTest. We keep a ActivityClassnameTest naming scheme.
+ * We are using robotium which is a different setup and we are using the LunarLander sample
+ * from the sdk as it has examples of common things you will run into such as UIThread testing, 
+ * some classes not visible, inner classes, etc.
  */
 public class BDDAndroidActivityTest extends ActivityInstrumentationTestCase2<BDDAndroidActivity>{
 
 	/** The solo. */
 	private Solo solo;
 	
+	
+	
 	/**
-	 * Instantiates a new bDD android activity test.
+	 * Instantiates a new BDD android activity test.
 	 *
-	 * @param pkg the pkg
-	 * @param BDDAndroidActivity the bDD android activity
 	 */
-	public BDDAndroidActivityTest(String pkg, Class BDDAndroidActivity) {
-		super(pkg, BDDAndroidActivity);
+	public BDDAndroidActivityTest() {
+		super("com.github.sharemebddandroid", BDDAndroidActivity.class);
 		// TODO Auto-generated constructor stub
 	}
-	
+	@UiThreadTest
 	public void testBDDAndroidActivity() {
-		solo.waitForActivity(BDDAndroidActivity.class.getSimpleName(), 5000);
+		solo.assertCurrentActivity(" BDDAndroidActivityis now current activity", BDDAndroidActivity.class);
+		//solo.waitForActivity(BDDAndroidActivity.class.getSimpleName(), 5000);
 	}
 	
-	/* (non-Javadoc)
-	 * @see android.test.ActivityInstrumentationTestCase2#setUp()
+	/** 
+	 *  We need to have robotium solo getInstrumnetation() before
+	 *  we issue anything having to do with activities. Otherwise will get
+	 *  errors of unable to resolve Activity class
+	 * (non-Javadoc)
+	 *   @see android.test.ActivityInstrumentationTestCase2#setUp()
 	 */
 	public void setUp() throws Exception {
-		solo = new Solo(getInstrumentation(), getActivity());
+		super.setUp();
+		solo = new Solo(getInstrumentation());
+		solo.getCurrentActivity();
 	}
     
+	
+
 	/* (non-Javadoc)
 	 * @see android.test.ActivityInstrumentationTestCase2#tearDown()
 	 */
