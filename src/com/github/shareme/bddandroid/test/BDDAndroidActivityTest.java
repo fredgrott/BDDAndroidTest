254743da13251700519d28facc6bef17e9676ad7
Fred Grott <fred.grott@gmail.com>
2012-01-21 10:31:57 -0600

    started first test class coding. Compiler/IDE asks for 1.5 compiler compliance when we use android mock/robotium

---
+++ src/com/github/shareme/bddandroid/test/BDDAndroidActivityTest.java
@@ -0,0 +1,16 @@
+package com.github.shareme.bddandroid.test;
+
+import com.github.shareme.bddandroid.BDDAndroidActivity;
+import com.jayway.android.robotium.solo.*;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class BDDAndroidActivityTest extends ActivityInstrumentationTestCase2<BDDAndroidActivity>{
+
+	private Solo solo;
+	
+	public BDDAndroidActivityTest(String pkg, Class BDDAndroidActivity) {
+		super(pkg, BDDAndroidActivity);
+		// TODO Auto-generated constructor stub
+	}
+
+}
